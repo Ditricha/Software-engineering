270073810c48472404675fbf48c352d1ce3fed5d
Ditricha <51129541+Ditricha@users.noreply.github.com>
2021-04-01 00:41:12 +0300

    Update integral.h

--- Numerical integration/Project2/integral.h
+++ Numerical integration/Project2/integral.h
@@ -1,10 +1,10 @@
 #pragma once
 
-// подынтегральные функции
+// РїРѕРґС‹РЅС‚РµРіСЂР°Р»СЊРЅС‹Рµ С„СѓРЅРєС†РёРёДЌ
 double f1(double);
 double f2(double);
 
-// методы
+// РјРµС‚РѕРґС‹
 double RectangleMethod(double a, double b, double eps, double(*f)(double));
 double TrapezoidalRule(double a, double b, double h, double(*f)(double));
 double DichotomyMethod(double a, double b, double eps, double(*f)(double));
